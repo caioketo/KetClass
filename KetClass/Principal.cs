c4d02d9e5e65b6e610dce0a04db8e27db7c0db48
Caio Keto <caionmoreno1@gmail.com>
2014-02-17 09:28:32 -0300

    c

---
+++ KetClass/Principal.cs
@@ -0,0 +1,29 @@
+﻿using KetClass.View.Alunos;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KetClass
+{
+    public partial class Principal : Form
+    {
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void alunosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (AlunoView view = new AlunoView())
+            {
+                view.ShowDialog();
+            }
+        }
+    }
+}
