61bb6ebccc920e37317c7fe5d38a01df78153ca3
Caio Keto <caionmoreno1@gmail.com>
2014-03-21 10:11:05 -0300

    c

--- KetClass/View/Shared/UCPesquisa.Designer.cs
+++ KetClass/View/Shared/UCPesquisa.Designer.cs
@@ -39,7 +39,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.tbxPesquisa.Location = new System.Drawing.Point(0, 0);
             this.tbxPesquisa.Name = "tbxPesquisa";
-            this.tbxPesquisa.Size = new System.Drawing.Size(53, 20);
+            this.tbxPesquisa.Size = new System.Drawing.Size(54, 20);
             this.tbxPesquisa.TabIndex = 0;
             this.tbxPesquisa.Leave += new System.EventHandler(this.tbxPesquisa_Leave);
             // 
@@ -47,9 +47,9 @@
             // 
             this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnClear.Location = new System.Drawing.Point(52, -1);
+            this.btnClear.Location = new System.Drawing.Point(53, -1);
             this.btnClear.Name = "btnClear";
-            this.btnClear.Size = new System.Drawing.Size(33, 22);
+            this.btnClear.Size = new System.Drawing.Size(21, 22);
             this.btnClear.TabIndex = 1;
             this.btnClear.Text = "X";
             this.btnClear.UseVisualStyleBackColor = true;
@@ -62,7 +62,7 @@
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.tbxPesquisa);
             this.Name = "UCPesquisa";
-            this.Size = new System.Drawing.Size(86, 20);
+            this.Size = new System.Drawing.Size(74, 20);
             this.ResumeLayout(false);
             this.PerformLayout();
 
